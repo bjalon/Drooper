93b71704326f2c2ac0ec97839734daae98914c01
Benjamin JALON <bjalon@nuxeo.com>
2015-06-27 21:24:00 +0200

    Initial commit from local git

---
+++ Droopper/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Droopper
+//
+//  Created by Benjamin JALON on 27/06/2015.
+//  Copyright (c) 2015 Benjamin JALON. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Droopper/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Droopper
+//
+//  Created by Benjamin JALON on 27/06/2015.
+//  Copyright (c) 2015 Benjamin JALON. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
